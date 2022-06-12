6b29241cf7af08fbcaf70318e0c2b64b4e7e90d6
aleksei <qqq0411@yandex.ru>
2022-06-13 00:17:09 +0300

    used SELF for correct self context in http.load() method

--- app.js
+++ app.js
@@ -9,3 +9,84 @@ http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
         console.log(posts);
     }
 })
+
+console.log(http);
+
+
+http.post('url', 'data', 'callback')
+
+
+
+
+
+// console.log(http);
+
+
+
+//function
+// function getThis() {
+//
+//     this.color = 'green'
+//     let team = 'ManUnited'
+//     console.log('1 - getThis THIS is : ' + this, this.color);
+//     console.log('2 - getThis THIS Color is : ' + this.color);
+//     console.log('4 - TEAM: ' + this.team);
+//
+//     console.dir(this)
+//     console.log(team);
+//
+//
+//
+// }
+
+
+// first invocation
+// console.log('first invocation')
+// let val = new getThis()
+// console.log('=====================');
+//
+// console.log('val: ', val);
+//
+//
+// console.log('=====================');
+
+// second invocation
+// console.log('second invocation')
+//
+// getThis();
+//
+// getThis();
+//
+// console.log(color)
+// console.log(team)
+//object
+const car = {
+    'wheel': 4,
+    'doors': 5,
+    toHorn: function () {
+        console.log('BEEEEP !', this);
+
+    },
+    toTurnOn: () =>  {
+        console.log('GO !', this);
+        this.getThis()
+    },
+    toTurnOff: ( )=> {
+        console.log('Stop !', this);
+        let toExitTheCar = function() {
+            console.log('Dont gorget to close the car');
+            console.log(this);
+        
+        }
+
+        toExitTheCar()
+    }
+}
+
+// car.toHorn()
+// car.toTurnOn()
+
+
+// car.toTurnOff()
+
+// console.log(car);
--- easyHttp.js
+++ easyHttp.js
@@ -1,28 +1,39 @@
-// easy http class
+// easyHTTP constructor
 function easyHTTP() {
     this.http = new XMLHttpRequest();
+    // console.log('This is: ' + this);
+
+    console.dir(this)
+
+    this.message = 'Hello World !'
 }
 
-easyHTTP.prototype.get = function(url, callback) {
-    this.http.open('GET', url, true);
-    this.http.onload = function() {
 
-        console.log(this);
-    
-    if(this.status === 200){
-        const posts = JSON.parse(this.responseText)
-       callback(null, posts)
-    } else {
-        callback('Error' + ' ' + this.status)
-    }
-       
-       
-       
-    //    console.log(this);
-    //    console.log(url);
+//Get request
+easyHTTP.prototype.get = function (url, callback) {
+
+    console.dir(this)
+    let self = this
+    this.http.open('GET', url, true);
+    this.http.onload = function () {
+        console.dir(this)
+
+        if (self.http.status === 200) {
+
+
+            const posts = JSON.parse(self.http.responseText)
+            callback(null, posts)
+        } else {
+            callback('Error' + ' ' + self.http.status)
+        }
     }
 
 
     this.http.send()
-    
+
+}
+
+//Post data
+easyHTTP.prototype.post = function (url, data, callback) {
+    console.log(url, data, callback);
 }
