a9e3ab6d5902884115ba7076607f788253b8fd60
aleksei <qqq0411@yandex.ru>
2022-06-14 14:28:39 +0300

    Delete request done

--- app.js
+++ app.js
@@ -6,37 +6,39 @@ const post = {
 }
 
 // get posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
-//
-//     // console.log(this);
-//     if (err) {
-//         console.error(err)
-//     } else {
-//         console.log(posts);
-//     }
-// })
+http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+
+    // console.log(this);
+    if (err) {
+        console.error(err)
+    } else {
+        console.log(posts);
+    }
+})
 
 //get single post
 
-// http.get('https://jsonplaceholder.typicode.com/posts/5', function (err, post) {
-//
-//     // console.log(this);
-//     if (err) {
-//         console.error(err)
-//     } else {
-//         console.log(post);
-//     }
-// })
+http.get('https://jsonplaceholder.typicode.com/posts/5', function (err, post) {
 
-// http.post('https://jsonplaceholder.typicode.com/posts', post, function (err, p) {
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             console.log(p)
-//         }
-//     }
-// )
+    // console.log(this);
+    if (err) {
+        console.error(err)
+    } else {
+        console.log(post);
+    }
+})
 
+//Send post
+http.post('https://jsonplaceholder.typicode.com/posts', post, function (err, p) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(p)
+        }
+    }
+)
+
+// Update post
 http.put('https://jsonplaceholder.typicode.com/posts/33', post, function (err, p) {
         if (err) {
             console.log(err)
@@ -46,87 +48,21 @@ http.put('https://jsonplaceholder.typicode.com/posts/33', post, function (err, p
     }
 )
 
+//Delete post
 
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, msg) {
 
-
-
-
-
-
-
-// console.log(http);
-
-
-//function
-// function getThis() {
-//
-//     this.color = 'green'
-//     let team = 'ManUnited'
-//     console.log('1 - getThis THIS is : ' + this, this.color);
-//     console.log('2 - getThis THIS Color is : ' + this.color);
-//     console.log('4 - TEAM: ' + this.team);
-//
-//     console.dir(this)
-//     console.log(team);
-//
-//
-//
-// }
-
-
-
-
-
-
-
-
-// first invocation
-// console.log('first invocation')
-// let val = new getThis()
-// console.log('=====================');
-//
-// console.log('val: ', val);
-//
-//
-// console.log('=====================');
-
-// second invocation
-// console.log('second invocation')
-//
-// getThis();
-//
-// getThis();
-//
-// console.log(color)
-// console.log(team)
-//object
-const car = {
-    'wheel': 4,
-    'doors': 5,
-    toHorn: function () {
-        console.log('BEEEEP !', this);
-
-    },
-    toTurnOn: () => {
-        console.log('GO !', this);
-        this.getThis()
-    },
-    toTurnOff: () => {
-        console.log('Stop !', this);
-        let toExitTheCar = function () {
-            console.log('Dont gorget to close the car');
-            console.log(this);
-
-        }
-
-        toExitTheCar()
+    // console.log(this);
+    if (err) {
+        console.error(err)
+    } else {
+        console.log(msg);
     }
-}
-
-// car.toHorn()
-// car.toTurnOn()
+})
 
 
-// car.toTurnOff()
 
-// console.log(car);
+
+
+
+
--- easyHttp.js
+++ easyHttp.js
@@ -1,10 +1,6 @@
 // easyHTTP constructor
 function easyHTTP() {
     this.http = new XMLHttpRequest();
-    // console.log('This is: ' + this);
-
-
-    this.message = 'Hello World !'
 }
 
 
@@ -25,11 +21,8 @@ easyHTTP.prototype.get = function (url, callback) {
         }
     }
 
-
     this.http.send()
-
 }
-
 
 //Send data
 easyHTTP.prototype.post = function (url, data, callback) {
@@ -59,7 +52,6 @@ easyHTTP.prototype.put = function (url, data, callback) {
     this.http.open('PUT', url, true);
     this.http.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     this.http.onload = function () {
-        // console.log(data)
         if (this.status === 200) {
             const ppp = JSON.parse(this.responseText)
             callback(null, ppp)
@@ -70,5 +62,24 @@ easyHTTP.prototype.put = function (url, data, callback) {
 
     this.http.send(JSON.stringify(data))
 
+}
 
+//Delete data
+
+easyHTTP.prototype.delete = function (url, callback) {
+    let self = this
+    this.http.open('DELETE', url, true);
+    this.http.onload = function () {
+
+        if (self.http.status === 200) {
+
+
+
+            callback(null, 'Post Deleted')
+        } else {
+            callback('Error, post has not been deleted' + ' ' + self.http.status)
+        }
+    }
+
+    this.http.send()
 }
